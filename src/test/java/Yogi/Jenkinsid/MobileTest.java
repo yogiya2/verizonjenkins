8d20a509e6c05505bd54218c547cfa9de672f2c6
Yogiya <yogiyathavasi2@gmail.com>
2023-07-26 14:14:42 +0530

    initial commait

---
+++ src/test/java/Yogi/Jenkinsid/MobileTest.java
@@ -0,0 +1,20 @@
+package Yogi.Jenkinsid;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+	
+	@Test
+	void login()
+	{
+		System.out.println("Login");
+	}
+	@Test
+	void logout()
+	{
+		System.out.println("logout");
+		//int num=10/0;
+	}
+
+
+}
